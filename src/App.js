4752287b2d1ae4091e0fe290f9f4963b50dda23e
Sarah Flowers <flowersfaerie@gmail.com>
2020-03-13 00:10:26 -0700

    Project started. Created all of the baseline files and coding. Still need to debug and style.

--- src/App.js
+++ src/App.js
@@ -1,13 +1,69 @@
 import React from 'react';
+import ReactDom from 'react-dom';
+
+import TodoList from './components/TodoList';
+import TodoForm from './components/TodoForm';
+import './components/Todo.css'
 
 class App extends React.Component {
+  constructor() {
+    super()
+    this.state = {
+      tasks
+    }
+  }
+
+  toggleDone = taskId => {
+    console.log("sf: App.js: App: toggleDone: taskId: ", taskId);
+    this.setState({
+      task: this.state.tasks.map(task => {
+        if (task.id === taskId) {
+          return {
+            ...task,
+            completed: !task.completed
+          };
+        }
+        return task;
+      })
+    });
+  };
+
+  addTask = taskName => {
+    this.setState({
+      tasks: [
+        ...this.state.tasks,
+        {
+          name: taskName,
+          completed: false,
+          id: (Math.random() * Math.random()).toString(9).substr(2,9)
+        }
+      ]
+    });
+  };
+
+  clearCompleted = () => {
+    console.log("sf: App.js: App: clearCompleted");
+    this.setState({
+      tasks: this.state.tasks.filter(task => {
+        return !task.completed;
+      })
+    });
+  };
   // you will need a place to store your state in this component.
   // design `App` to be the parent component of your application.
   // this component is going to take care of state, and any change handlers you need to work with your state
   render() {
     return (
-      <div>
-        <h2>Welcome to your Todo App!</h2>
+      <div className="App">
+        <div className="header">
+          <h2>Welcome to your Todo App!</h2>
+          <TodoForm addTask={this.addTask} />
+        </div>
+        <TodoList
+          tasks={this.state.tasks}
+          toggleCompleted={this.toggleCompleted}
+          clearCompleted={this.clearCompleted}
+        />
       </div>
     );
   }
---
+++ src/components/TodoForm.js
@@ -0,0 +1,36 @@
+import React from 'react';
+
+class TodoForm extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            taskName: ""
+        };
+    }
+
+    handleChanges = e => {
+        this.setState({ taskName: e.target.value });
+    };
+
+    handleAddTask = e => {
+        e.preventDefault();
+        this.props.addTask(this.state.taskName);
+    };
+
+    render() {
+        console.log("sf: TodoForm.js: render: rendering form");
+        return (
+            <form onSubmit={this.handleAddTask}>
+                <input
+                    type="text"
+                    name="task"
+                    value={this.state.taskName}
+                    onChange={this.handleChanges}
+                />
+                <button>Add Task</button>
+            </form>
+        );
+    }
+}
+
+export default TodoForm;
